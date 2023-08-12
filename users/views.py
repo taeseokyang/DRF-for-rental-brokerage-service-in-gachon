fa9b634f080d1a2149a9f0f496285f9c403d6de7
taeseokyang <ts.yang.0123@gmail.com>
2023-08-13 01:39:51 +0900

    Commit

---
+++ users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
